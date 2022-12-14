16d761e2007e5ebed31e08d2f14c9f52445a8e82
reyvidod <reyvidod@gmail.com>
2022-12-14 22:12:07 +0700

    install axios, implement get all posts, format code with prettier plugin

--- src/components/featuredPost/FeaturedPost.jsx
+++ src/components/featuredPost/FeaturedPost.jsx
@@ -1,57 +1,45 @@
 import React from "react";
 import "./featuredpost.css";
+import Posts from "../posts/Posts";
 
 import { BsArrowDownShort } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
-const FeaturedPost = () => {
-	return (
-		<section>
-			<div className="container-fluid featured-post">
-				<div className="heading">
-					<h1>Latest Update</h1>
-					<BsArrowDownShort className="icon" size="6rem" />
-				</div>
-				<div className="row latest-update">
-					<div className="col-12">
-						<Link to="/post/test" className="link">
-							<div className="card">
-								<img src="https://images.pexels.com/photos/1109354/pexels-photo-1109354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="card-img-top" alt="..."></img>
-								<div className="card-body">
-									<p className="card-text">Post title</p>
-								</div>
-							</div>
-						</Link>
-					</div>
-				</div>
-				<div className="row featured">
-					<div className="heading">
-						<h2>Featured</h2>
-					</div>
-					<div className="col-6">
-						<Link to="/post/test" className="link">
-							<div className="card">
-								<img src="https://images.pexels.com/photos/311458/pexels-photo-311458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="card-img-top" alt="..."></img>
-								<div className="card-body">
-									<p className="card-text">Post title</p>
-								</div>
-							</div>
-						</Link>
-					</div>
-					<div className="col-6">
-						<Link to="/post/test" className="link">
-							<div className="card">
-								<img src="https://images.pexels.com/photos/383568/pexels-photo-383568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="card-img-top" alt="..."></img>
-								<div className="card-body">
-									<p className="card-text">Post title</p>
-								</div>
-							</div>
-						</Link>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+const FeaturedPost = ({ posts }) => {
+  return (
+    <section>
+      <div className="container-fluid featured-post">
+        <div className="heading">
+          <h1>Latest Update</h1>
+          <BsArrowDownShort className="icon" size="6rem" />
+        </div>
+        <div className="row latest-update">
+          <div className="col-12">
+            <Link to="/post/test" className="link">
+              <div className="card">
+                <img
+                  src="https://images.pexels.com/photos/1109354/pexels-photo-1109354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                  className="card-img-top"
+                  alt="..."
+                ></img>
+                <div className="card-body">
+                  <p className="card-text">Post title</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className="row featured">
+          <div className="heading">
+            <h2>Featured</h2>
+          </div>
+          {posts.map((p) => (
+            <Posts post={p} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FeaturedPost;
---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+const Posts = ({ post }) => {
+  return (
+    <div className="col-6">
+      <Link to={`/post/${post._id}`} className="link">
+        <div className="card">
+          <img src={post.image} className="card-img-top" alt="..."></img>
+          <div className="card-body">
+            <p className="card-text">{post.title}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Posts;
