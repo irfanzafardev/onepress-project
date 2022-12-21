3fa861f74939587653825691a7f241fc9a31c18c
reyvidod <reyvidod@gmail.com>
2022-12-21 21:10:58 +0700

    implement get post by id. refactor code using prettier extension

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,49 +1,47 @@
-// import axios from "axios";
-// import React, { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useLocation } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 import Comments from "../comment/Comments";
 import "./post.css";
 
 const Post = () => {
-	// const { currentUser } = useSelector((state) => state.user);
-	// const dispatch = useDispatch();
+  // const { currentUser } = useSelector((state) => state.user);
+  // const dispatch = useDispatch();
 
-	// const path = useLocation().pathname.split("/")[2];
-	// const [post, setPost] = useState({});
-	// const [author, setAuthor] = useState({});
+  const path = useLocation().pathname.split("/")[2];
+  const [post, setPost] = useState({});
+  const [author, setAuthor] = useState({});
 
-	// useEffect(() => {
-	// 	const fetchData = async () => {
-	// 		try {
-	// 			const postRes = await axios.get("/posts/");
-	// 		} catch (error) {}
-	// 	};
-	// });
-	return (
-		<section className="post">
-			<div className="container-fluid">
-				<div className="post-header">
-					<img src="https://images.pexels.com/photos/383568/pexels-photo-383568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="post-img"></img>
-				</div>
-				<div className="post-title">
-					<h1>Post Title</h1>
-				</div>
-				<div className="post-detail">
-					<h3>Author</h3> <span>|</span>
-					<h3>01.01.01</h3>
-				</div>
-				<div className="post-body">
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<br />
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				</div>
-				<div className="post-comment">
-					<Comments />
-				</div>
-			</div>
-		</section>
-	);
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await axios.get("http://localhost:3001/posts/" + path);
+      setPost(res.data);
+    };
+    getPost();
+  }, [path]);
+  return (
+    <section className="post">
+      <div className="container-fluid">
+        <div className="post-header">
+          {post.photo && <img src={post.photo} alt="post-img"></img>}
+        </div>
+        <div className="post-title">
+          <h1>{post.title}</h1>
+        </div>
+        <div className="post-detail">
+          <h3>{post.username}</h3> <span>|</span>
+          <h3>{new Date(post.createdAt).toDateString()}</h3>
+        </div>
+        <div className="post-body">
+          <p>{post.desc}</p>
+        </div>
+        <div className="post-comment">
+          <Comments />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Post;
